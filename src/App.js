03fd99a9fc367bc556923b3f4da1c6be863152c0
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2021-01-07 00:32:13 -0700

    Created a HTML basics to a Weather App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Weather from "./Weather";
+
 
 export default  function App() {
   return (
     <div className="App">
       <div class="container">
-      <h1> Weather App </h1> 
+        
+        <Weather />
       <footer>
         This project was coded by Aksana Kurbanova and is <a href="https://github.com/Sberry2020/react-final-project" target="_blank" rel="noreferrer">
           open-sourced on GitHub 
