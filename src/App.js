46e6710baf8f5da549c8f105bb92efccda9c61b9
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2021-01-06 23:36:21 -0700

    Added a bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default  function App() {
   return (
     <div className="App">
+      <div class="container">
       <h1> Weather App </h1> 
       <footer>
         This project was coded by Aksana Kurbanova and is <a href="https://github.com/Sberry2020/react-final-project" target="_blank" rel="noreferrer">
           open-sourced on GitHub 
    </a>
-      </footer>
+        </footer>
+        </div>
     </div>
   );
 }
