bd04d466cd22ccae5dae9d95e4a02235e68bc168
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2021-01-06 22:58:56 -0700

    Added on GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default  function App() {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App </h1> 
+      <footer>
+        This project was coded by Aksana Kurbanova and is <a href="https://github.com/Sberry2020/react-final-project" target="_blank" rel="noreferrer">
+          open-sourced on GitHub 
+   </a>
+      </footer>
     </div>
   );
 }
