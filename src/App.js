7ecc1398cdf97aae1530d83772c838d5fa135c0f
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2021-01-11 01:39:43 -0700

    Added an API integration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
-
-export default  function App() {
+export default function App() {
   return (
     <div className="App">
       <div class="container">
-        
-        <Weather />
-      <footer>
-        This project was coded by Aksana Kurbanova and is <a href="https://github.com/Sberry2020/react-final-project" target="_blank" rel="noreferrer">
-          open-sourced on GitHub 
-   </a>
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by Aksana Kurbanova and is{" "}
+          <a
+            href="https://github.com/Sberry2020/react-final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and on Netlify App is{" "}
+          <a
+            href="https://laughing-bohr-32dea7.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Netlify
+          </a>
         </footer>
-        </div>
+      </div>
     </div>
   );
 }
-
-
